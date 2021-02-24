066d091b9a168e758973315140815fc7a452ac52
Your Name <you@example.com>
2021-02-25 00:58:17 +0900

    Reindent 4 space

--- src/topcoder/SRM801/HouseNumbering.java
+++ src/topcoder/SRM801/HouseNumbering.java
@@ -5,15 +5,15 @@ package topcoder.SRM801;
 import java.util.*;
 
 public class HouseNumbering {
-	public int[] prepareDigits(int fh, int n) {
-		int[] cnt = new int[10];
-		for (int i = 0; i < n; ++i) {
-			int x = i * 2 + fh;
-			while (x > 0) {
-				++cnt[x % 10];
-				x /= 10;
-			}
-		}
-		return cnt;
-	}
+    public int[] prepareDigits(int fh, int n) {
+        int[] cnt = new int[10];
+        for (int i = 0; i < n; ++i) {
+            int x = i * 2 + fh;
+            while (x > 0) {
+                ++cnt[x % 10];
+                x /= 10;
+            }
+        }
+        return cnt;
+    }
 }
